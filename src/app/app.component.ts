f0aad1f547735febca4d3dce6b0f155726acb350
motheblackcat <motheblackcat@gmail.com>
2021-02-10 11:58:30 +0100

    dynamic links with anchors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'mbc';
+export class AppComponent implements OnInit {
+  @HostListener('window:scroll')
+  onScrollHost() {
+    const divs = document.querySelectorAll('div');
+    divs.forEach((div: HTMLElement) => {
+      const check = div.getBoundingClientRect().y >= -600 && div.getBoundingClientRect().y <= 400;
+      const link = document.querySelector(`a[href="#${div.id}"]`) as HTMLElement;
+      link.style.color = check ? 'white' : 'grey';
+    });
+  }
+
+  ngOnInit() {
+    this.onScrollHost();
+  }
 }
