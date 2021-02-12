31f0fa2e8776efb03640bdf5db8b21f7f7558d0f
motheblackcat <motheblackcat@gmail.com>
2021-02-12 14:54:58 +0100

    Add links and icons

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
 })
-export class IntroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class IntroComponent {
+  links = [
+    { url: 'https://angular.io/', icon: 'fab fa-angular fa-3x', class: 'angular' },
+    { url: 'https://reactjs.org/', icon: 'fab fa-react fa-3x', class: 'react' },
+    { url: 'https://vuejs.org/', icon: 'fab fa-vuejs fa-3x', class: 'vue' },
+    { url: 'https://unity.com/solutions/game', icon: 'fab fa-unity fa-3x', class: 'unity' },
+  ];
 }
