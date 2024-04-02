009f764c4be9d4c079e7bddbc9b420954d6362a5
motheblackcat <morad.elhamdouni@gmail.com>
2024-04-02 16:16:39 +0200

    WIP card component

--- src/app/components/work/card/card.component.ts
+++ src/app/components/work/card/card.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { LANGUAGES_COLORS, LANGUAGES_NAMES } from 'src/app/enums/languages.enum';
+import { Repo } from 'src/app/interfaces/repo.interface';
+
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  // TODO: Check if imports property is need
-  imports: [],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  // TODO: Make sure it's non-null or undefined value
+  repo = input<Repo>();
 
+  getDotColor(language: string | undefined): string {
+    switch (language) {
+      case LANGUAGES_NAMES.TYPESCRIPT:
+        return LANGUAGES_COLORS.TYPESCRIPT;
+
+      case LANGUAGES_NAMES.JAVASCRIPT:
+        return LANGUAGES_COLORS.JAVASCRIPT;
+
+      case LANGUAGES_NAMES.CSHARP:
+        return LANGUAGES_COLORS.CSHARP;
+
+      default:
+        return LANGUAGES_COLORS.UNKNOWN;
+    }
+  }
 }
--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,11 +1,11 @@
 import { Observable, of } from 'rxjs';
-import { LANGUAGES_COLORS, LANGUAGES_NAMES } from 'src/app/enums/languages.enum';
 import { Repo } from 'src/app/interfaces/repo.interface';
 import { mockRepos } from 'src/app/mocks/repos.mock';
 import { GithubService } from 'src/app/services/github.service';
 
-import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
 import { CardComponent } from './card/card.component';
 
 @Component({
@@ -23,20 +23,4 @@ export class WorkComponent {
   // repos$: Observable<Repo[]> | undefined = this.gs.getRepos();
   zen$: Observable<string> | undefined = of('Mock!');
   repos$: Observable<Repo[]> | undefined = of(mockRepos);
-
-  getDotColor(language: string): string {
-    switch (language) {
-      case LANGUAGES_NAMES.TYPESCRIPT:
-        return LANGUAGES_COLORS.TYPESCRIPT;
-
-      case LANGUAGES_NAMES.JAVASCRIPT:
-        return LANGUAGES_COLORS.JAVASCRIPT;
-
-      case LANGUAGES_NAMES.CSHARP:
-        return LANGUAGES_COLORS.CSHARP;
-
-      default:
-        return LANGUAGES_COLORS.UNKNOWN;
-    }
-  }
 }
